2de822a71472dffb28829814264e5fce564680d6
Martin Lenart <martin_lenart@icloud.com>
2022-01-10 20:37:07 +0100

    Add project files.

---
+++ Hello World Flyout/Hello World Flyout/Globals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Hello_World_Flyout.Models;
+namespace Hello_World_Flyout
+{
+    public class Globals
+    {
+        #region Lazy implementation
+        Globals() { } // just to avoid any direct instantiations
+
+        private static Lazy<Globals> _instance = new Lazy<Globals>(() => new Globals());
+        public static Globals Data => _instance.Value;
+        #endregion
+
+        #region Data implementation 
+        // accessible as Globals.Data.xx 
+        public List<City> Citylist { get; set; }
+        public string Message { get; set;}
+        public DateTime Time { get; set;}
+        #endregion
+    }
+}
